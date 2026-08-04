e5405995eaf8ce0ad444e747adf6fe822d012247
yimmy_Alex <yimmy120alexander@gmail.com>
2026-08-03 20:23:37 -0500

    se creo js kilos

--- script.js
+++ script.js
@@ -30,9 +30,75 @@
 
 // pedir una temperatura en c y convertirla en f;
 
-let celsius = Number ( prompt ('escribe los grados celsius'))
-let farenjeith = celsius * 1.8 + 32
+// let celsius = Number ( prompt ('escribe los grados celsius'))
+// let farenjeith = celsius * 1.8 + 32
 
-console.log( farenjeith)
+// console.log( farenjeith)
 
-// pedir una distancia en km y convertirla a m; 
+// pedir una distancia en km y convertirla a m;         
+
+// let precio = prompt('escriba un precio')
+// let descuento = precio * 0.80 ;
+
+// console.log (descuento)
+
+// let nota1 = Number (prompt('escriba la primera nota'))
+
+// let nota2 = Number (prompt('escriba la segunda nota'))
+
+// let nota3 = Number (prompt('escriba la tercera nota'))
+
+
+// let promedio = (nota1 + nota2 + nota3) / 3
+
+// console.log( promedio)
+
+// si el precio es menor a 100 dolares, no hay descuento y hay cobro por envio
+// pero si el precio es entre 100 y 200 hay un 20 % de descuento y el envio es gratis
+// si es mayor a 200, hay un descuento del 30%
+
+
+// let precio = prompt(' escriba el precio');
+
+// if  ( precio <= 100 ){
+//     console.log('no tienes descuento y contara con cobro adicional por envio')
+// }else if( precio >=100 && precio <=200){
+//     console.log('Tienes un 20% de descuento y el envio es gratis')
+// }else{
+//     console.log('tienes un descuento por 30%')
+// }
+
+
+// let precios = [100, 200, 155, 348, 56];
+
+
+
+
+// for (let i = 0; i < 5; i++) {
+//     let precio = precios[i]
+//     if (precio <= 100) {
+//         console.log('No tienes descuento');
+//     } else if (precio > 100 && precio <= 200) {
+//         console.log('Tienes un descuento del 20%')
+//     } else {
+//         console.log('Tienes un descuento del 30%')
+//     }
+// }
+
+
+let peso = prompt ('ingrese su peso');
+
+let libras = ( peso *2.205);
+
+console.log( 'su peso en libras es ' + libras)
+
+if ( libras < 160){
+    console.log( 'su peso es demasiado bajo para competir')
+}else if( libras >=160 && libras <= 180 ){
+ console.log( ' su peso es medio para competir, participe!!')
+} else { libras > 180
+    console.log( 'su peso es mayor puede competir')
+};
+
+
+
