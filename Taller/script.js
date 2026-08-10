f765878b383433cc15d5474c87e6562f51af5b95
yimmy_Alex <yimmy120alexander@gmail.com>
2026-08-10 18:58:06 -0500

    se realizo el segundo ejercicio

---
+++ Taller/script.js
@@ -0,0 +1,25 @@
+// ejercicio n°1 
+
+
+// let colores = [ 'blanco', 'negro', 'gris', 'azul', 'dorado']
+
+// for (let i = 0; i <= colores.length ; i++) {
+
+    
+// console.log( colores[i] )
+// }
+
+//  console.log (  ' primer color' +  colores[0])
+
+
+//ejercicio n° 2
+
+const numeros = [ 96, 45, 59, 5, 66, 99]
+
+
+for (let i = 0; i <= numeros.length; i++) {
+     
+    console.log( `el array tiene este numero de elementos ` + numeros.length)
+    
+}
+
