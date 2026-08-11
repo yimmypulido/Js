c68a12bbeaae664c2bada818e38a860a8ca50268
yimmy_Alex <yimmy120alexander@gmail.com>
2026-08-10 19:04:50 -0500

    se realizo el tercer ejercicio

--- Taller/script.js
+++ Taller/script.js
@@ -12,14 +12,24 @@
 //  console.log (  ' primer color' +  colores[0])
 
 
-//ejercicio n° 2
+//ejercicio n°2
 
-const numeros = [ 96, 45, 59, 5, 66, 99]
+// const numeros = [ 96, 45, 59, 5, 66, 99]
 
 
-for (let i = 0; i <= numeros.length; i++) {
+// for (let i = 0; i <= numeros.length; i++) {
      
-    console.log( `el array tiene este numero de elementos ` + numeros.length)
+//     console.log( `el array tiene este numero de elementos ` + numeros.length)
     
-}
+// }
 
+// ejercicio n°3
+
+
+for (let i = 1; i <= 20 ; i++) {
+    
+ if ( i % 2 === 0){
+
+    console.log(i)
+ }
+}
