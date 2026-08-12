803fc75786f181f790672c4cea903a3feeb16b14
yimmy_Alex <yimmy120alexander@gmail.com>
2026-08-11 19:21:18 -0500

    se quitaron for inecesarios

--- Taller/script.js
+++ Taller/script.js
@@ -3,28 +3,21 @@
 
 // let colores = [  'blanco', 'negro', 'gris', 'azul', 'dorado',]
 
-// for (let i = 0; i <= colores.length ; i++) {
-
-// }
 //     console.log( colores[colores.length - 1] )
 //  console.log (  ' primer color ' +  colores[0])
  
 
 //ejercicio n°2
 
-const numeros = [ 96, 45, 59, 5, 66, 99]
+// const numeros = [ 96, 45, 59, 5, 66, 99]
 
+// } console.log( `el array tiene este numero de elementos ` + numeros.length)
 
-for (let i = 0; i <= numeros.length; i++) {
-     
-    
-} console.log( `el array tiene este numero de elementos ` + numeros.length)
 
 // ejercicio n°3
 
 
-// for (let i = 1; i <= 20 ; i++) {
-    
+// for (let i = 1; i <= 20 ; i++) {git
 //  if ( i % 2 === 0){
 
 //     console.log(i)
